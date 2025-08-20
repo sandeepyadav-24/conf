400bde785bdd07d5ca9e150aaf6a01c650da56e3
sandeepyadav-24 <103882286+sandeepyadav-24@users.noreply.github.com>
2025-08-20 08:39:34 +0530

    Resolved error

--- src/Components/Body.tsx
+++ src/Components/Body.tsx
@@ -1,6 +1,6 @@
 //import { Link } from "react-router-dom";
 import Counter from "./Counter";
-import { useState } from "react";
+
 import { Link } from "react-router-dom";
 const Body = () => {
   return (
