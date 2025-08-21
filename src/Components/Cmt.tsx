f8ba07a633af145bed8333a1b3ad7fad2f546971
sandeepyadav-24 <103882286+sandeepyadav-24@users.noreply.github.com>
2025-08-21 09:10:36 +0530

    Added Correct Msg

--- src/Components/Cmt.tsx
+++ src/Components/Cmt.tsx
@@ -13,7 +13,7 @@ const CmtAcknowledgement = () => {
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-2xl w-full">
         <div className="prose prose-gray max-w-none">
           <p className="text-gray-700 leading-relaxed text-lg">
-            The Microsoft CMT services was used for managing the peer-reviewing
+            The Microsoft CMT service was used for managing the peer-reviewing
             process for this conference. This service was provided for free by
             Microsoft and they bore all expenses, including costs for Azure
             cloud services as well as for software development and support.
